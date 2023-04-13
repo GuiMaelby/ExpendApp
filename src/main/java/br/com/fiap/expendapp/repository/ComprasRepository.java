00871f2aa4acfe57a91b26700c0534b2d0ab17f7
Guilherme Maelby <107452235+GuiMaelby@users.noreply.github.com>
2023-04-13 19:51:22 -0300

    Delete src directory

--- src/main/java/br/com/fiap/expendapp/repository/ComprasRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.expendapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.fiap.expendapp.models.Compras;
-
-public interface ComprasRepository extends JpaRepository<Compras, Long> {
-
-}
