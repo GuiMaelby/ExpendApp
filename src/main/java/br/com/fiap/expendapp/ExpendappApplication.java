19da45289c27c986ae0e0093323765656df908b8
Guilherme Maelby <107452235+GuiMaelby@users.noreply.github.com>
2023-05-04 16:43:05 -0300

    Add files via upload

---
+++ src/main/java/br/com/fiap/expendapp/ExpendappApplication.java
@@ -0,0 +1,14 @@
+package br.com.fiap.expendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpendappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpendappApplication.class, args);
+	}
+
+
+}
---
+++ src/main/java/br/com/fiap/expendapp/models/Conta.java
@@ -0,0 +1,29 @@
+package br.com.fiap.expendapp.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Conta {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String nome;
+    @NotNull
+    private String email;
+    @NotNull
+    private String senha;
+    @NotNull
+    private LocalDate dataNascimento;    
+}
